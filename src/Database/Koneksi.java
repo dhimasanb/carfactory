748804024f2187df27bbacd1136bdb8c547e79bf
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2016-12-24 11:04:25 +0700

    konfigurasi database mysql

---
+++ src/Database/Koneksi.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import com.mysql.jdbc.Driver;
+
+/**
+ *
+ * @author Griya Salbini
+ */
+public class Koneksi {
+    public static Connection koneksi;
+    public static Connection getConnection() throws SQLException {
+        if(koneksi==null){
+            new Driver();
+            koneksi =DriverManager.getConnection("jdbc:mysql://localhost:3306/pabrikmobil","root","root");
+        }
+        return koneksi;
+    }
+    public static void main(String[] args){
+        try{
+            getConnection();
+ JOptionPane.showMessageDialog(null,"koneksi berhasil","report koneksi",JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (SQLException ex){
+            System.err.println("Koneksi GAGAL");
+        }
+    }
+}
